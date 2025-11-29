35c75a81426287fdc9d0d23d4f59e52c1fe245b4
adamzulu123 <120639376+adamzulu123@users.noreply.github.com>
2025-11-29 18:54:38 +0100

    first api

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+games = {}
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI!"}
@@ -8,3 +10,33 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "query": q}
+
+
+@app.get("/gameState?user={userName}")
+def get_game_state(user_name: str):
+    if user_name not in games:
+        return {
+            "data" : {
+                "gameState": "bet",
+                "playerBalance": 1000 # domyślny balans
+            }
+        }
+    return games[user_name].to_response()
+
+@app.post("/placeBet")
+def placeBets():
+    return ''
+
+@app.post("/hit")
+def hit():
+    return ''
+
+@app.post("/stand")
+def standBets():
+    return ''
+
+@app.post("/nextTurn")
+def nextTurn():
+    return ''
+
+
