7de9aecfdc2093e5d6ad8e7f82bd45379b638305
Clwmm <emgie4432@gmail.com>
2025-12-06 15:32:11 +0100

    add ut script

--- ops/backend/test.sh
+++ ops/backend/test.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo PASSED
+set -e
+
+if [[ "$OSTYPE" == "msys" ]]; then
+    VENV_ACTIVATE="backend/.venv/Scripts/activate"
+else
+    VENV_ACTIVATE="backend/.venv/bin/activate"
+fi
+
+if [ ! -d "backend/.venv" ]; then
+    echo "❌ Brak środowiska .venv! Uruchom najpierw build.sh"
+    exit 1
+fi
+
+echo "=== Aktywacja środowiska ==="
+source "$VENV_ACTIVATE"
+
+echo "=== Uruchamianie aplikacji ==="
+python -m unittest discover -s backend/tests -p "test_*.py" -v
